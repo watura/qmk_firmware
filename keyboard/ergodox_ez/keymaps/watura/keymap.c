a78b103600b54f450a9d5173351c07774919a02e
Wataru NISHIMOTO <watura@g91.org>
2016-06-06 07:20:13 +0900

    ctrl

--- keyboard/ergodox_ez/keymaps/watura/keymap.c
+++ keyboard/ergodox_ez/keymaps/watura/keymap.c
@@ -15,7 +15,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |  tab   |   Q  |   W  |   E  |   R  |   T  |  L1  |           |  L1  |   Y  |   U  |   I  |   O  |   P  |   \    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * |CTRL    |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |; / L2|  ENTER |
- * |--------+------+------+------+------+------| ESC  |           | Meh  |------+------+------+------+------+--------|
+ * |--------+------+------+------+------+------| ESC  |           | CTR  |------+------+------+------+------+--------|
  * | LShift |Z/Ctrl|   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |//Ctrl|' / Cmd |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   |Grv/L1|  '"  |AltShf|   UP | GUI  |                                       |  GUI | Down |   [  |   ]  | ~L1  |
@@ -23,9 +23,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                        ,-------------.       ,-------------.
  *                                        | App  | LGui |       | Alt  |Ctrl/Esc|
  *                                 ,------|------|------|       |------+--------+------.
- *                                 |      |      | Home |       | PgUp |        |      |
----|  Enter | Space|
- *                                 |      | space| End  |       | PgDn |        |      |
+ *                                 |      |      | alt  |       | PgUp |        |      |
+ *                                 |space | back |------|       |------|  Enter | Space|
+ *                                 |      | space|enter |       | bs   |        |      |
  *                                 `--------------------'       `----------------------'
  */
 // If it accepts an argument (i.e, is a function), it doesn't need KC_.
@@ -38,17 +38,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LSFT,        KC_Z,  KC_X,   KC_C,   KC_V,   KC_B,   KC_ESC,
         LT(SYMB,KC_GRV),KC_QUOT,      LALT(KC_LSFT),  KC_UP ,KC_LGUI,
                                               ALT_T(KC_APP),  KC_LGUI,
-                                                              KC_HOME,
-                                               KC_SPC,KC_BSPC,KC_END,
+                                                              KC_LALT,
+                                               KC_SPC,KC_BSPC,KC_ENT,
         // right hand
              KC_RGHT,     KC_6,   KC_7,   KC_8,   KC_9,   KC_0,             KC_MINS,
              TG(SYMB),    KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,             KC_BSLS,
                           KC_H,   KC_J,   KC_K,   KC_L,   LT(MDIA, KC_SCLN),KC_ENT,
-             MEH_T(KC_NO),KC_N,   KC_M,   KC_COMM,KC_DOT, CTL_T(KC_SLSH),   GUI_T(KC_QUOT),
+             KC_RCTRL,KC_N,   KC_M,   KC_COMM,KC_DOT, CTL_T(KC_SLSH),   GUI_T(KC_QUOT),
                                   KC_RCTRL,KC_DOWN,KC_LBRC,KC_RBRC,          KC_FN1,
-             KC_LALT,        CTL_T(KC_ESC),
+             KC_LALT,        KC_ESC,
              KC_PGUP,
-             KC_PGDN, KC_ENT, KC_SPC
+             KC_BSPC, KC_ENT, KC_SPC
     ),
 /* Keymap 1: Symbol Layer
  *
