7db4119bddd235a73164e82fcca4f76052cfe3d7
Wataru Nishimoto <mail@wtr.io>
2018-12-05 08:05:35 +0900

    fortitude60

--- keyboards/fortitude60/keymaps/watura/config.h
+++ keyboards/fortitude60/keymaps/watura/config.h
@@ -27,7 +27,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define MASTER_LEFT
 // #define MASTER_RIGHT
 #define EE_HANDS
-#define TAPPING_TERM 200
+#define TAPPING_TERM 120
 #define PERMISSIVE_HOLD
 
 /* #undef RGBLED_NUM */
